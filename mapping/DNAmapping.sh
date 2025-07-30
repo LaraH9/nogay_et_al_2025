32798894bae4c3ed3b4cb3516d257833010d1b9a
LaraH9 <75339995+LaraH9@users.noreply.github.com>
2025-07-30 14:46:34 +0200

    Update DNAmapping.sh

--- mapping/DNAmapping.sh
+++ mapping/DNAmapping.sh
@@ -1,1 +1,2 @@
 
+DNAmapping --DAG --mapq 1 -i fastq/ -o Output_spikein --trim --fastqc --properPairs --dedup --cutntag --alignerOpts="--local --very-sensitive-local --no-discordant --no-mixed -I 10 -X 700" hybrid_adjusted.yaml 
